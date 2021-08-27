849c1304e39d9f424e8dfcf159c6df45b896781d
Rayna Ratcliff <raynaratcliff@hotmail.com>
2021-08-26 20:13:25 -0400

    creating the rows for the time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,7 @@ var today = moment().format("dddd, MMMM Do");
 var now = moment().format("H A");
 
 /* planWorkday entries for each hour of the workday */
-var planWorkday = [
+var planner = [
   { time: "9 AM", event: "" },
   { time: "10 AM", event: "" },
   { time: "11 AM", event: "" },
@@ -18,3 +18,21 @@ var planWorkday = [
   { time: "4 PM", event: "" },
   { time: "5 PM", event: "" },
 ];
+
+/*Current Day*/ 
+$("currentDay").text(today); 
+
+/*Create rows */
+planner.forEach(function(timeBlock, index) {
+    var label = timeBlock; 
+    var blockColor = colorRow(label);
+    var row = '<div class="time-block" id="' + index +
+    '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+        label + '</div><textarea class="form-control ' + blockColor + '">' + timeBlock.event +
+        '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
+
+    /* Adding rows to container div */
+    $(".container").append(row);
+
+}); 
+
